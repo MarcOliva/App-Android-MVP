91c8156918f7aa26f671a8aceee0af28132b35b3
marc <ing.oliva@hotmail.com>
2019-09-08 23:55:43 -0500

    add details

--- app/src/main/java/com/oliva/marc/appmvp/view/DetailCountryActivity.kt
+++ app/src/main/java/com/oliva/marc/appmvp/view/DetailCountryActivity.kt
@@ -2,12 +2,30 @@ package com.oliva.marc.appmvp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ahmadrosid.svgloader.SvgLoader
 import com.oliva.marc.appmvp.R
+import com.oliva.marc.appmvp.model.Country
+import kotlinx.android.synthetic.main.activity_detail_country.*
 
 class DetailCountryActivity : AppCompatActivity() {
 
+    lateinit var country:Country
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_country)
+        supportActionBar?.hide()
+        country = intent.extras?.get("country") as Country
+
+        name_country_textview.text = country.name
+        SvgLoader.pluck()
+            .with(this)
+            .setPlaceHolder(R.drawable.loading_spinner,R.drawable.loading_spinner)
+            .load(country.uriFlag, flag_imageview)
+        capital_textview.text = country.capital
+        population_textview.text= country.population
+        region_textview.text=country.region
+        code_textview.text= country.alpha3Code
+        area_textview.text= country.area
+
     }
 }
